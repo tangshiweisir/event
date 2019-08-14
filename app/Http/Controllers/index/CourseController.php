778f5710b2298fa9eab717c1d5249b8997a7c667
yourname <1542157146@qq.com>
2019-08-14 09:39:45 +0800

    zcy

--- app/Http/Controllers/index/CourseController.php
+++ app/Http/Controllers/index/CourseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\index;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class CourseController extends Controller
 {
